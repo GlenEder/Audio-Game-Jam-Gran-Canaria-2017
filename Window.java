96198504ccb5a447f85946a9ca84feec073d79f0
Glen Michael Austin Eder <geder@purdue.edu>
2017-10-13 15:49:04 -0400

    Adds graphic to screen
    Graphic says that game is best played with headphones on

--- Window.java
+++ Window.java
@@ -1,19 +1,30 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class Window extends JFrame{
+public class Window extends JPanel{
 	
 	private int width = 600;
 	private int height = 400;
 
 	public Window() {
-		super("Can You Hear Me?");
-		setSize(width, height);
-		setLocationRelativeTo(null);
-		setResizable(false);
-		getContentPane().setBackground(java.awt.Color.black);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Can You Hear Me?");
+		frame.setSize(width, height);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.getContentPane().setBackground(java.awt.Color.black);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(this);
 
-		setVisible(true);
+		frame.setVisible(true);
+		repaint();
+	}
+
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, width, height);
+		g.setColor(Color.green);
+		g.setFont(new Font("Impact", Font.BOLD, 24));
+		g.drawString("This game is best played with headphones on", 50, 200);
 	}
 
 }
